eea6840471b48f7c5990318901168235c9ebf17e
DebanjanSarkar <debanjanwork21@gmail.com>
2022-08-12 00:03:55 +0530

    Attempting link fix

--- PGLife/includes/database_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $db_hostname = "127.0.0.1";
-  $db_username = "root";
-  $db_password = "";
-  $db_name = "pg_life";
-
-  $con = mysqli_connect( $db_hostname , $db_username , $db_password , $db_name );
-?>
